82cadb3aa9b541806590b405a9b5b99f3f792e05
Seyit <seyitaligermany@gmail.com>
2023-11-21 16:31:53 +0100

    added WebDemoShop main

---
+++ src/test/java/com/eurotech/pages/BasePage.java
@@ -0,0 +1,23 @@
+package com.eurotech.pages;
+
+import com.eurotech.utilities.BrowserUtils;
+import com.eurotech.utilities.Driver;
+import com.eurotech.utilities.ExcelUtil;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.Map;
+
+public abstract class BasePage { // BasePage uzerinden obje olusturmayacagimiz icin abstract yaptik
+    public BasePage() {
+        PageFactory.initElements(Driver.get(),this);
+    }
+    WebDriverWait wait=new WebDriverWait(Driver.get(),10);
+
+
+}
---
+++ src/test/java/com/eurotech/stepDefinitions/Hooks.java
@@ -0,0 +1,38 @@
+package com.eurotech.stepDefinitions;
+
+import com.eurotech.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.util.concurrent.TimeUnit;
+
+public class Hooks { // kanca demek.Butun testlerimizden once bu kancayi takiyoruz
+    // Before ve after method annotion lari gordugun de direkt calistirir
+    // class in isminin bir onemi yok
+    @Before
+    public void setUp(){
+       // System.out.println("\tThis is coming from Before Method");
+        Driver.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Driver.get().manage().window().maximize();
+    }
+    @After
+    public void tearDown(Scenario scenario){  // screenshot almak icin  olusturduk
+            if(scenario.isFailed()){
+                final byte [] screenschot=((TakesScreenshot)Driver.get()).getScreenshotAs(OutputType.BYTES);
+                scenario.attach(screenschot,"image/png","screenshot");
+            }
+       // System.out.println("\tThis is coming from After Method");
+        Driver.closeDriver();
+    }
+    @Before("@db") // tag uzerinden secilen test senaryolari icin aktif hala gelebilir.
+    public void setUpDB(){
+        //System.out.println("\tConnecting DB");
+    }
+    @After("@db")
+    public void tearDownDB(){
+        //System.out.println("\tDisconnecting DB");
+    }
+}
